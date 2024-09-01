5b7615ec5ca7d51f8676ddd587c71f9c90e11ee9
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-01 20:20:31 +0600

    version 1

---
+++ Python/Average.py
@@ -0,0 +1,12 @@
+tree1 = 11
+tree2 = 94
+tree3 = 98
+tree4 = 69
+tree5= 34
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+
+print("The sum of the of the five numbers is : " , sum)
+
+res = sum / 5
+
+print("The average is : " , res)
