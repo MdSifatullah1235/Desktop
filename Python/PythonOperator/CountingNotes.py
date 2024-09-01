ec420cb2878c8f900fdd011185a33af723746cb8
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-01 21:02:19 +0600

    version 1

--- Python/PythonOperator/CountingNotes.py
+++ Python/PythonOperator/CountingNotes.py
@@ -1,10 +1,10 @@
-amount_input = float(input("Enter a amount within the multiplication of 100, 50 or 10 taka to withdraw : "))
+amount_input = float(input("Enter an amount within the multiplication of 100, 50 or 10 taka to withdraw: "))
 print(amount_input)
 
 note1 = amount_input // 100
 note2 = (amount_input % 100) // 50
-note3 = ((amount_input % 100) % 50)  // 10
+note3 = ((amount_input % 100) % 50) // 10
 
-print("The amount of 100 taka notes" , note1)
-print("The amount of 50 taka notes" , note2)
-print("The amount of 10 taka notes" , note3)
+print("The amount of 100 taka notes:", int(note1))
+print("The amount of 50 taka notes:", int(note2))
+print("The amount of 10 taka notes:", int(note3))
