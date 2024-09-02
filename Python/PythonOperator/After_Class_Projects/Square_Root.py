47a8e417c45a35f24e85fa844be761c610d9cf87
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-02 18:01:57 +0600

    version 1

--- Python/PythonOperator/After_Class_Projects/Square_Root.py
+++ Python/PythonOperator/After_Class_Projects/Square_Root.py
@@ -1,5 +1,3 @@
-num_input =  int(input("Enter a number to square root : "))
-print(num_input)
-num = num_input
-num /= num_input
-print("The square root of " , num_input , "is" , num)
+num_input = int(input("Enter a number to square root: "))
+num = num_input ** 0.5
+print("The square root of", num_input, "is", num)
