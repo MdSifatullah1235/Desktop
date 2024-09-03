460dc2a1aabe75eccfe1f354ba6f9e0d98c4c0b1
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-03 19:56:01 +0600

    version 1

--- Python/ProfitorLoss.py
+++ Python/ProfitorLoss.py
@@ -1,4 +1,10 @@
 cost = float(input("Enter the actual product price : "))
-sales = float(input("Enter the sales amount : "))
+sales = float(input("Enter the sale amount : "))
 
-if 
+if (sales > cost):
+    amount_1 = sales - cost
+    print("The profit is {} ".format(amount_1))
+
+else:
+    amount_2 = cost - sales
+    print("The loss is {} ".format(amount_2))
