18362f52c9ed12e1c96f9af7663451db4a7d1a4a
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-04 16:27:40 +0600

    version 1

--- Python/Con_Stat/After_Class_Projects/Divisible_By_2_and_3_or_not.py
+++ Python/Con_Stat/After_Class_Projects/Divisible_By_2_and_3_or_not.py
@@ -3,9 +3,9 @@ num_input = int(input("Enter a number :"))
 num = num_input % 10
 
 
-if (num % 3 == 0):
-    print(num_input , "is divisible by 3")
+if (num % 6 == 0):
+    print(num_input , "is divisible by 3 and 2")
 
 
 else:
-    print(num_input , "is not divisible by 3")
+    print(num_input , "is not divisible by 3 and 2")
