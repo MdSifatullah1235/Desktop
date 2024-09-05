0c4ada94e188aac89ea28bbbfd015f3b2113b508
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-05 20:42:55 +0600

    version 1

---
+++ Python/PythonOperator_2/PythonOperator1_Project.py
@@ -0,0 +1,15 @@
+a = 10
+b = -15
+
+if a > 0 and b > 0:
+    print("The numbers are all greater than 0")
+
+else:
+    print("The numbers are all not greater than 0")
+
+
+if a > 0 or b > 0:
+    print("One of these number is greater than 0")
+
+else:
+    print("One of these number is not gretaer than 0")
