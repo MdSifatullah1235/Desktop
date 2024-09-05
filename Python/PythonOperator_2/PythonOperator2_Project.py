41cc8c0654e8dd294e5345edf5592aa44b65aa78
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-05 20:48:33 +0600

    version 1

---
+++ Python/PythonOperator_2/PythonOperator2_Project.py
@@ -0,0 +1,12 @@
+a = 10
+b = 12
+c = 16
+
+if a > b and a > c:
+    print("a greater than b and c")
+
+elif b > a and b > c:
+    print("b is greater than a and c")
+
+elif c > a and c > b:
+    print("c is greater than a and b")
