0d9c961cf3ccba21d2661d81f2fc8dea8059154e
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-06 10:30:03 +0600

    version 1

---
+++ Python/PythonOperator_2/After_Class_Projects/Project.py
@@ -0,0 +1,17 @@
+a = 10
+b = 10
+c = 10
+
+if a > b and a > c:
+    print("a is greater than b and c")
+
+elif b > a and b > c:
+    print("b is greater than a and c")
+
+elif c > a and c > b:
+    print("c is greater than a and b")
+
+elif a == b and a == c:
+    print("a, b and c are equals")
+
+elif a == b and a >
