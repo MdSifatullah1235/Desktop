258be2d876cb234a01d7d1f9422a34f6bcdb0b51
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-10 15:00:50 +0600

    version 1

--- Python/PythonOperator_2/After_Class_Projects/Project.py
+++ Python/PythonOperator_2/After_Class_Projects/Project.py
@@ -1,18 +1,18 @@
-a = int(input("Enter a number for the value (a) :"))
-b = int(input("Enter a number for the value (b) :"))
-c = int(input("Enter a number for the value (c) :"))
+a = 10
+b = 10
+c = 10
 
-if a > b and a > c:
-    print("a is greater than b and c")
+if a == b and a == c:
+    print("a, b, and c are equal.")
+
+elif a > b and a > c:
+    print("a is greater than b and c.")
 
 elif b > a and b > c:
-    print("b is greater than a and c")
+    print("b is greater than a and c.")
 
 elif c > a and c > b:
-    print("c is greater than a and b")
-
-elif a == b and a == c:
-    print("a, b and c are equals")
+    print("c is greater than a and b.")
 
 elif a == b:
     if a > c:
