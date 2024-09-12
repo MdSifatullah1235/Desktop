709ced75272213fadc7c20acedd7fecbb6a2cc62
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-12 18:35:55 +0600

    version 1

--- Python/PythonOperator_2/After_Class_Projects/Project.py
+++ Python/PythonOperator_2/After_Class_Projects/Project.py
@@ -3,30 +3,35 @@ b = int(input("Enter a number for the value (b): "))
 c = int(input("Enter a number for the value (c): "))
 
 if a == b and a == c:
-    print("a, b, and c are equal.")
+    print("a, b, and c are equal")
 
 elif a == b:
     if a > c:
-        print("a and b are equal, but c is lower.")
+        print("a and b are equal, but c is lower")
+        
     else:
-        print("a and b are equal, but c is greater.")
+        print("a and b are equal, but c is greater")
 
 elif b == c:
     if b > a:
-        print("b and c are equal, but a is lower.")
+        print("b and c are equal, but a is lower")
+
     else:
-        print("b and c are equal, but a is greater.")
+        print("b and c are equal, but a is greater")
 
 elif a == c:
     if a > b:
-        print("a and c are equal, but b is lower.")
+        print("a and c are equal, but b is lower")
+
     else:
-        print("a and c are equal, but b is greater.")
+        print("a and c are equal, but b is greater")
 
 else:
     if a > b and a > c:
-        print("a is greater than b and c.")
+        print("a is greater than b and c")
+
     elif b > a and b > c:
-        print("b is greater than a and c.")
+        print("b is greater than a and c")
+
     elif c > a and c > b:
-        print("c is greater than a and b.")
+        print("c is greater than a and b")
