2f4ca6f05b27e9e5b8d2014514d2af7226e15e08
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-12 18:33:54 +0600

    version 1

--- Python/PythonOperator_2/After_Class_Projects/Project.py
+++ Python/PythonOperator_2/After_Class_Projects/Project.py
@@ -1,33 +1,28 @@
-a = int(input("Enter a number for the value (a) :"))
-b = int(input("Enter a number for the value (b) :"))
-c = int(input("Enter a number for the value (c) :"))
+if a == b and a == c:
+    print("a, b, and c are equal.")
 
-if a > b and a > c:
-    print("a is greater than b and c")
+elif a == b:
+    if a > c:
+        print("a and b are equal, but c is lower.")
+    else:
+        print("a and b are equal, but c is greater.")
 
-elif b > a and b > c:
-    print("b is greater than a and c")
+elif b == c:
+    if b > a:
+        print("b and c are equal, but a is lower.")
+    else:
+        print("b and c are equal, but a is greater.")
 
-elif c > a and c > b:
-    print("c is greater than a and b")
+elif a == c:
+    if a > b:
+        print("a and c are equal, but b is lower.")
+    else:
+        print("a and c are equal, but b is greater.")
 
-elif a == b and a == c:
-    print("a, b and c are equals")
-
-elif a == b and a > c:
-    print("a and b are equals but c is lower")
-
-elif b == c and b > a:
-    print("b and c are equals but a is lower")
-
-elif a == c and a > b:
-    print("a and c are equals but b is lower")
-
-elif a == b and a < c:
-    print("a and b are equals but c is greater")
-
-elif b == c and b < a:
-    print("b and c are equals but a is greater")
-
-elif a == c and a < b:
-    print("a and c are equals but b is greater")
+else:
+    if a > b and a > c:
+        print("a is greater than b and c.")
+    elif b > a and b > c:
+        print("b is greater than a and c.")
+    elif c > a and c > b:
+        print("c is greater than a and b.")
