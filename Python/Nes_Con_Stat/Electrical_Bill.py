b98cf5a72030571992de2096bea6efba4e75cbf5
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-12 21:25:00 +0600

    version 1

--- Python/Nes_Con_Stat/Electrical_Bill.py
+++ Python/Nes_Con_Stat/Electrical_Bill.py
@@ -1,21 +1,21 @@
-units = float(input("Enter the amount of units consumed :"))
+units = float(input("Enter the amount of units consumed: "))
 
 if units < 50:
     unit_cost = units * 2.60
     tax = 25
 
-elif units < 100:
+elif units <= 100:
     unit_cost = 130 + ((units - 50) * 3.25)
     tax = 35
 
-elif units < 200:
-    unit_cost = 130 + 162.50 ((units - 100) * 5.26)
+elif units <= 200:
+    unit_cost = 130 + 162.50 + ((units - 100) * 5.26)
     tax = 45
 
 else:
-    unit_cost = 130 + 162.50 + 526 ((units - 200) * 8.45)
+    unit_cost = 130 + 162.50 + 526 + ((units - 200) * 8.45)
     tax = 75
 
 total = unit_cost + tax
 
-print("The total electrical bill is :" , total)
+print("The total electrical bill is: %.2f" %total)
