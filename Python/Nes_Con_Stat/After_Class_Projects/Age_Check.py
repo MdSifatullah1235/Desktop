9c805702887ac415b1f857a6e955bc51a2bacf53
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-13 15:00:25 +0600

    version 1

--- Python/Nes_Con_Stat/After_Class_Projects/Age_Check.py
+++ Python/Nes_Con_Stat/After_Class_Projects/Age_Check.py
@@ -2,10 +2,14 @@ inp = int(input("Enter your age :"))
 
 if inp >= 10:
     if inp > 20:
-        print("Your too old")
+        print("Your older than 20 you cannot enter")
+        if inp >= 40:
+            print("Also your very old ")
 
     else:
         print("You can enter")
 
 else:
-    print("Your too young")
+    print("Your too younger than 10 you cannot enter")
+    if inp > 5:
+        print("Also you are a little kid")
