161430be115b2a156cbe9705d0b2d64e191332b8
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-16 20:54:27 +0600

    version 1

--- Python/Loops/Project2.py
+++ Python/Loops/Project2.py
@@ -1,5 +1,9 @@
-inp = int(input("Enter a number :"))
-print("Numbers from {0} to {1} are :".format(inp,1))
+# inp = int(input("Enter a number :"))
+# print("Numbers from {0} to {1} are :".format(inp,1))
+# for i in range(inp,0,-1):
+#    print(i)
 
-for i in range(inp,0,-1):
+inp = int(input("Enter a number :"))
+
+for i in range(2,inp + 1,):
     print(i)
