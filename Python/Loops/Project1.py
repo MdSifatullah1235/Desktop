2f2e7acf0d45f1590ea054489bd1269ea435e000
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-16 20:21:32 +0600

    version 1

--- Python/Loops/Project1.py
+++ Python/Loops/Project1.py
@@ -1,7 +1,12 @@
-name = "Sifatullah"
+# name = "Sifatullah"
 
-for c in name:
-    print(c)
+# for c in name: print(c)
 
-for n in range(1,10):
-    print(n)
+# for n in range(1,10): print(n)
+
+inp = int(input("Enter a number : "))
+
+sum = 0 
+
+for b in range(inp):
+    print(b)
