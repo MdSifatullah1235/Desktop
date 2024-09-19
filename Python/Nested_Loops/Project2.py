9f1cc77dfee103ddf33884cb91e953216dbcf28c
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-19 20:08:01 +0600

    version 1

--- Python/Nested_Loops/Project2.py
+++ Python/Nested_Loops/Project2.py
@@ -1,6 +1,6 @@
 lower = int(input("Enter a number for the lower range :"))
 upper = int(input("Enter a number for the upper range :"))
-
+print("The prime numbers between {} and {} are :".format(lower,upper))
 for num in range(lower,upper + 1):
   if num > 1:
     for i in range(2,num):
