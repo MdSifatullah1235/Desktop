665f1f26539f0e4fa55bbc3b174d817980c8069a
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-19 18:27:38 +0600

    version 1

--- Python/While Loops/After_Class_Projects/Project1.py
+++ Python/While Loops/After_Class_Projects/Project1.py
@@ -1,7 +1,9 @@
 num = int(input("Enter a number: "))
 count = 0
 temp = num
+
 while temp > 0:
     temp //= 10  
     count += 1
-print("The number of digits in", num, "is", count)
+    
+print("The number of digits in {} is {}".format(num,count))
