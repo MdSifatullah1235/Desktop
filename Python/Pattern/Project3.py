8f35abb63dad96d5d35299cdd479323b5273af85
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-24 20:53:31 +0600

    version 1

--- Python/Pattern/Project3.py
+++ Python/Pattern/Project3.py
@@ -4,6 +4,6 @@ for i in range(1 , n + 1):
     for j in range(n - i):
         print(" " , end="")
     
-    for k in range(1 , 2 * 1):
+    for k in range(1 , 2 * i):
         print("*" , end="")
-print()
+    print()
