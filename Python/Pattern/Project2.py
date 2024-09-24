cc20777a8a80816e851c33a452e325e3471aa0fb
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-24 20:29:05 +0600

    version 1

--- Python/Pattern/Project2.py
+++ Python/Pattern/Project2.py
@@ -4,7 +4,7 @@ n = int(input("Enter the amount:"))
 count = 1
 
 for i in range(1):
-    for j in range(count + 1):
-        print(count, end="")
+    for j in range(i + 1):
+        print("{} ".format(count), end="")
         count +=1
     print()
