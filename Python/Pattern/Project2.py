bd5ca7fc94367dc47052b0a302d0341ec04722e6
MdSifatullah1235 <miasifat1235@gmail.com>
2024-09-24 20:34:35 +0600

    version 1

--- Python/Pattern/Project2.py
+++ Python/Pattern/Project2.py
@@ -1,10 +1,9 @@
-print("Enter a amount of numbers:")
-n = int(input("Enter the amount:"))
+n = int(input("Enter the amount: "))
 
 count = 1
 
-for i in range(1):
+for i in range(n):
     for j in range(i + 1):
-        print("{} ".format(count), end="")
-        count +=1
+        print(count, end=" ")
+        count += 1
     print()
