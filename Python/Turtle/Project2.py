fe2773fadd23097e0a2467379540893c69123a10
MdSifatullah1235 <miasifat1235@gmail.com>
2024-10-01 20:01:13 +0600

    version 1

--- Python/Turtle/Project2.py
+++ Python/Turtle/Project2.py
@@ -2,10 +2,28 @@ import turtle
 turtle.Screen().bgcolor("blue")
 turtle.Screen().setup(400,600)
 
-triangle = turtle.Turtle()
+star = turtle.Turtle()
 
-for i in range(3):
-    triangle.forward(70)
-    triangle.left(120)
+star.forward(100)
+
+star.left(120)
+star.forward(100)
+
+star.left(120)
+star.forward(100)
+
+star.penup()
+star.right(150)
+star.forward(50)
+
+star.pendown()
+star.right(90)
+star.forward(100)
+
+star.right(120)
+star.forward(100)
+
+star.right(120)
+star.forward(100)
 
 turtle.done()
