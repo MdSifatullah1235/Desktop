faf75dd6d4c5565fa7065b484e4b221b9fcf511c
MdSifatullah1235 <miasifat1235@gmail.com>
2024-10-02 20:27:27 +0600

    version 1

---
+++ Python/Turtle/After_Class_Projects/AP1.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(400,600)
+
+diamond = turtle.Turtle()
+
+diamond.forward(75)
+
+diamond.right(45)
+diamond.forward(100)
+
+diamond.right(90)
+diamond.forward(150)
+
+diamond.right(90)
+diamond.forward(150)
+
+diamond.right(90)
+diamond.forward(100)
+
+turtle.done()
