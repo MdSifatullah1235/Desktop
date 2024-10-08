d173cd8dcfef3421af49b98265843319ece5da2d
MdSifatullah1235 <miasifat1235@gmail.com>
2024-10-08 19:51:42 +0600

    version 1

--- Python/Arguments/Project2.py
+++ Python/Arguments/Project2.py
@@ -1,4 +1,5 @@
 def factorial(x):
+    """This is a recursive function"""
     if x == 0 or x == 1:
         return 1
     else:
@@ -8,5 +9,3 @@ print("The factorial of 0 is :", factorial(0))
 print("The factorial of 1 is :", factorial(1))
 print("The factorial of 2 is :", factorial(2))
 print("The factorial of 5 is :", factorial(5))
-
-
