94196181d1749e75e24e455c4a33fbac784e54cf
MdSifatullah1235 <miasifat1235@gmail.com>
2024-10-08 19:44:15 +0600

    version 1

---
+++ Python/Arguments/Project2.py
@@ -0,0 +1,10 @@
+def factorial(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x *factorial(x -1)
+    
+print("The factorial of 0 is :", factorial(0))
+print("The factorial of 1 is :", factorial(1))
+print("The factorial of 2 is :", factorial(2))
+print("The factorial of 5 is :", factorial(5))
