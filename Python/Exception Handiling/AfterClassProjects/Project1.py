83fd462ef23d53502b732b98777c7a4ca2eda0f8
MdSifatullah1235 <miasifat1235@gmail.com>
2024-10-16 18:42:47 +0600

    version 1

--- Python/Exception Handiling/AfterClassProjects/Project1.py
+++ Python/Exception Handiling/AfterClassProjects/Project1.py
@@ -9,4 +9,11 @@ except:
 
 else:
     print("All Ok")
-    
+    valid = False
+    while not valid:
+        if num1 % 2 == 0:
+            print("This Number Is An Even Number!")
+            valid = True
+        elif num1 % 3 == 0:
+            print("This Number Is An Odd Number!")
+            valid = True
