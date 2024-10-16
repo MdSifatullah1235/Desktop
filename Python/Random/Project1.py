c74c3644117b3c4719bb98e1628905e5c23b7387
MdSifatullah1235 <miasifat1235@gmail.com>
2024-10-16 19:20:05 +0600

    version 1

--- Python/Random/Project1.py
+++ Python/Random/Project1.py
@@ -1,11 +1,12 @@
 import random
-number = input("Enter a number:")
+
 play = True
 guess = str(random.randint(10,20))
 
 print("I will generate a number from 10 to 20 Try to guess it!")
 
 while play:
+    number = input("Enter a number:")
     if number == guess:
         print("You guessed it!")
         print("The Number Was",number)
